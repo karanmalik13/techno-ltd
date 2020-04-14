4f8cc9c3371cdd3358204f0b06a6584078477597
Karan Malik <karan13ov@gmail.com>
2020-04-14 15:24:34 +0530

    Final Commit 1.0

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Employee} from '../employee';
+
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  @Input() employee:Employee;
+
+  powers = ['Smart Coder', 'Super Flexible',
+  'Logical Thinker', 'Excellent Analyst'];
+  model = new Employee(18, '', '', '');
+
+
+submitted=false;
+
+onSubmit()
+{
+  this.submitted=true;
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+ 
+  }
+
+}
